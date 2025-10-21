d422ce4451a57d1f42c5a59f5ed36e8d49407408
Bencsik Tamás László <bencsik.tamas05@gmail.com>
2025-10-21 20:36:07 +0200

    Coverage

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -23,7 +23,7 @@ public class GT4500 implements SpaceShip {
 
     public boolean fireLaser(FiringMode firingMode) {
         // TODO not implemented yet
-        return false;
+        return false; // ezt nem járta be
     }
 
     /**
